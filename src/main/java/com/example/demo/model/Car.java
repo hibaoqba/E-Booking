0e8057a5df50da0f43df8eb17be61c7a7e1f973b
GhizlaneMoudane <ghizmoudane@gmail.com>
2024-03-15 11:59:37 +0000

    car features

---
+++ src/main/java/com/example/demo/model/Car.java
@@ -0,0 +1,34 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import jakarta.websocket.Encoder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="car")
+public class Car {
+    @Id
+    @GeneratedValue
+    private String id;
+    private String brand;
+    private String model;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private int year;
+    private boolean availability;
+    private int price;
+    @ElementCollection
+    private List<String> images = new ArrayList<>();
+    @Embedded
+    private CarFeatures carFeatures;
+
+}
