b2a5ef195443cee281e24359d006555af392d9a6
GhizlaneMoudane <ghizmoudane@gmail.com>
2024-03-15 13:23:17 +0000

    reservation

---
+++ src/main/java/com/example/demo/model/Reservation.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    private String id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
